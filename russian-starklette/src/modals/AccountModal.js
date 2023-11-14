53b0dd179bcad80ff17e3062724b2dc6491734b1
mohiiit <mohit18343@iiitd.ac.in>
2023-11-14 20:03:03 +0530

    account module updated

--- russian-starklette/src/modals/AccountModal.js
+++ russian-starklette/src/modals/AccountModal.js
@@ -1,92 +1,63 @@
 import React, { useEffect, useState } from 'react';
-import {
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  TextField,
-  Slide,
-} from '@mui/material';
-
+import { Dialog, DialogTitle, DialogContent, Button, CircularProgress, Typography } from '@mui/material';
 import { useGame } from '../context/ProviderContext';
 
+const styles = {
+  dialogContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '16px',
+  },
+  loader: {
+    margin: '8px',
+  },
+  button: {
+    marginTop: '16px',
+  },
+};
+
+
+
 function AccountModal({ open, onClose }) {
-  const [showAddAccountForm, setShowAddAccountForm] = useState(false);
-  const [accountAddress, setAccountAddress] = useState('');
-  const [privateKey, setPrivateKey] = useState('');
-  const [activeAccount, setActiveAccount] = useState(localStorage.getItem('address'));
-  const [isConnected, setIsConnected] = useState(false);
 
-  const {provider, connectWallet, disconnectWallet, setNewAccount} = useGame();
+  const {provider, connectWallet, disconnectWallet, setNewAccount, isConnected, account} = useGame();
+  
 
-  const handleAddAccount = () => {
-    setNewAccount({address: accountAddress});
-  };
-
-  // useEffect(() => {
-  //   const address = localStorage.getItem('address');
-
-  //   if (address) {
-  //       setNewAccount({address: address});
-  //   }
-
-  // }, [provider]);
-
-  return (
+  return  (
     <Dialog open={open} onClose={onClose}>
-      {
-        isConnected ? (
-          <>
+      {isConnected ? (
+        <>
           <DialogTitle>Connected</DialogTitle>
-          <p><button onClick={()=> {disconnectWallet()}}>Disconnect</button></p>
-      <DialogContent>
-        <div>
-          <strong>Active Account:</strong> {activeAccount}
-        </div>
-        <Button
-          variant="outlined"
-          onClick={() => setShowAddAccountForm(!showAddAccountForm)}
-        >
-          Add New Account
-        </Button>
-        {showAddAccountForm && <Slide direction="up" in={showAddAccountForm}>
-          <div>
-            <TextField
-              label="Account Address"
-              variant="outlined"
-              fullWidth
-              value={accountAddress}
-              onChange={(e) => setAccountAddress(e.target.value)}
-            />
-            <TextField
-              label="Private Key"
-              variant="outlined"
-              fullWidth
-              value={privateKey}
-              onChange={(e) => setPrivateKey(e.target.value)}
-            />
+          <DialogContent style={styles.dialogContent}>
+            <Typography variant="body1" gutterBottom>
+              <strong>Active Account:</strong> {account}
+            </Typography>
             <Button
               variant="contained"
-              color="primary"
-              onClick={handleAddAccount}
+              color="secondary"
+              onClick={() => disconnectWallet()}
+              style={styles.loader}
             >
-              Add New Account
+              Disconnect
             </Button>
-          </div>
-        </Slide>}
-      </DialogContent>
-          </>
-        ) : (
-          <div>
-          <span>Choose a wallet:</span>
-          <p>
-            <button onClick={() => connectWallet()}>Connect a Wallet</button>
-          </p>
-        </div>
-        )
-      }
-      
+          </DialogContent>
+        </>
+      ) : (
+        <DialogContent style={styles.dialogContent}>
+          <Typography variant="h6" gutterBottom>
+            Choose a wallet
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => connectWallet()}
+            style={{ ...styles.loader, ...styles.button }}
+          >
+            Connect a Wallet
+          </Button>
+        </DialogContent>
+      )}
     </Dialog>
   );
 }
